a7bc31bdc7dd0717922d45e8438b8d8a91eb8363
Sarvagya Pant <sarvagya@Sarvagyas-MacBook-Pro.local>
2017-05-11 11:37:22 -0500

    started Week 1

---
+++ Week1/anyint/solver.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from random import randrange
+
+def solve_it(input_data):
+    # return a positive integer, as a string
+    return randrange(7,20)
+
+if __name__ == '__main__':
+    print('This script submits the integer: %s\n' % solve_it(''))
+
